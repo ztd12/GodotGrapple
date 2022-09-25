e840c323b465a0e481ea62fb17390fb9745b993a
varunika320 <66332471+varunika320@users.noreply.github.com>
2022-09-25 17:56:09 -0500

    movements

---
+++ Player.gd
@@ -0,0 +1,43 @@
+extends RigidBody2D
+
+
+# Declare member variables here. Examples:
+export var speed = 400
+var screen_size
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("jump"):
+		$AnimatedSprite.animation = "jump"
+	if Input.is_action_pressed("attack"):
+		$AnimatedSprite.animation = "fight"
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized() *speed
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+	
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+	
+	if velocity.x != 0:
+		$AnimatedSprite.animation = "run"
+		$AnimatedSprite.flip_v = false
+		$AnimatedSprite.flip_h = velocity.x < 0
+
+		
+	
