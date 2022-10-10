f6e9181508b4b8176453f9aac6c6dd69987a757b
varunika320 <66332471+varunika320@users.noreply.github.com>
2022-10-09 23:09:07 -0500

    pause partially working

--- ForestLevel/Scenes/ForestLevel.gd
+++ ForestLevel/Scenes/ForestLevel.gd
@@ -3,12 +3,16 @@ extends Node2D
 
 func _on_PauseButton_pressed():
 	get_tree().paused = true
-	$Pause_Popup.show()
+	$Pause_Menu.show()
 
 
 func _on_Quit_pressed():
 	get_tree().change_scene("res://HUD/Level Menu.tscn")
 
 func _on_Resume_pressed():
-	$Pause_Popup.hide()
+	$Pause_Menu.hide()
 	get_tree().paused = false
+
+
+func _on_Restart_pressed():
+	get_tree().change_scene("res://ForestLevel/Scenes/ForestLevel.tscn")
