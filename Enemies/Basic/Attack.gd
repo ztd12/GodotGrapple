acfb60088003f836084cceebafd76cba8ebbbcd8
joseh <jose_javi_89@hotmail.com>
2022-10-23 22:39:01 -0500

    more clear attacks pt 2

--- Enemies/Basic/Attack.gd
+++ Enemies/Basic/Attack.gd
@@ -11,10 +11,18 @@ func enter(_msg := {}) -> void:
 		state_machine.transition_to("Idle")
 	animated_sprite.play("attack")
 	
+func update(delta):
+	if animated_sprite.get_frame() == 3:
+		animated_sprite.modulate = Color(100,1,1,50)
+	if animated_sprite.get_frame() > 4:
+		animated_sprite.modulate = Color(1,1,1,1)		
+	
 func physics_update(delta: float) -> void:
 	if owner.taking_damage:
+		animated_sprite.modulate = Color(1,1,1,1)
 		state_machine.transition_to("Takehit")
 	if owner.dead:
+		animated_sprite.modulate = Color(1,1,1,1)
 		state_machine.transition_to("Death")
 		
 	if animated_sprite.get_frame() == 5:
@@ -22,18 +30,22 @@ func physics_update(delta: float) -> void:
 	
 	if animated_sprite.get_frame() == 7:
 		hitbox.monitorable = false
+		animated_sprite.modulate = Color(1,1,1,1)
 		state_machine.transition_to("Idle")
 	
 	if owner.detected_player == true and owner.distance_to_player > 45:
 		hitbox.monitorable = false
+		animated_sprite.modulate = Color(1,1,1,1)
 		state_machine.transition_to("Run")
 		
 	if not owner.is_on_floor():
 		hitbox.monitorable = false
+		animated_sprite.modulate = Color(1,1,1,1)
 		state_machine.transition_to("Fall")
 		return 
 		
 	if owner.detected_player == false:
 		hitbox.monitorable = false
+		animated_sprite.modulate = Color(1,1,1,1)
 		state_machine.transition_to("Idle")
 	
