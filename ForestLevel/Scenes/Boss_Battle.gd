60e5d7560d8dc5e7fd0c704c3aa9645b8b70afc4
varunika320 <66332471+varunika320@users.noreply.github.com>
2022-11-07 14:19:58 -0600

    adding lives to boss levels

--- ForestLevel/Scenes/Boss_Battle.gd
+++ ForestLevel/Scenes/Boss_Battle.gd
@@ -2,10 +2,17 @@ extends Node2D
 
 
 func _ready():
-	pass
+	Global.lives = Global.max_lives
+	load_hearts()
+	Global.hud = self
 	
 	
 
 func _on_Boss_tree_exited():
 	yield(get_tree().create_timer(3), "timeout")
 	get_tree().change_scene("res://HUD/Game Won.tscn")
+
+func load_hearts():
+	$hearts.rect_size.x = Global.lives * 600
+	$unfilledhearts.rect_size.x = (Global.max_lives - Global.lives) * 305
+	$unfilledhearts.rect_position.x = $hearts.rect_position.x + $hearts.rect_size.x * $hearts.rect_scale.x
