7fb800044eeb0d192c6c97ccd6c8e42367840448
joseh <jose_javi_89@hotmail.com>
2022-11-14 15:34:18 -0600

    tutorial fix

---
+++ Tutorial/tutorial2b.gd
@@ -0,0 +1,13 @@
+extends Control
+
+func _ready():
+	Global.lives = Global.max_lives
+	load_hearts()
+	Global.hud = self
+
+#func _on_Boss_tree_exited():
+
+func load_hearts():
+	$hearts.rect_size.x = Global.lives * 600
+	$unfilledhearts.rect_size.x = (Global.max_lives - Global.lives) * 305
+	$unfilledhearts.rect_position.x = $hearts.rect_position.x + $hearts.rect_size.x * $hearts.rect_scale.x
