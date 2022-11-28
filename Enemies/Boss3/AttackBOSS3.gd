e192153e7a921c31990d0e6ee9ce0a5b5e1e68ea
joseh <jose_javi_89@hotmail.com>
2022-11-28 14:09:19 -0600

    boss 3

---
+++ Enemies/Boss3/AttackBOSS3.gd
@@ -0,0 +1,50 @@
+extends State
+
+export(NodePath) var _animation
+onready var animated_sprite: AnimatedSprite = get_node(_animation)
+
+export(NodePath) var _hitbox
+onready var hitbox: Area2D = get_node(_hitbox)
+
+func enter(_msg := {}) -> void:
+	animated_sprite.play("attack")
+	
+func update(_delta):
+	if animated_sprite.get_frame() == 3:
+		animated_sprite.modulate = Color(100,1,1,50)
+		yield(get_tree().create_timer(.3), "timeout")
+	if animated_sprite.get_frame() > 4:
+		animated_sprite.modulate = Color(1,1,1,1)		
+	
+func physics_update(_delta: float) -> void:
+	if owner.taking_damage:
+		animated_sprite.modulate = Color(1,1,1,1)
+		state_machine.transition_to("Takehit")
+	if owner.dead:
+		animated_sprite.modulate = Color(1,1,1,1)
+		state_machine.transition_to("Death")
+		
+	if animated_sprite.get_frame() == 5:
+		hitbox.monitorable = true
+	
+	if animated_sprite.get_frame() == 7:
+		hitbox.monitorable = false
+		animated_sprite.modulate = Color(1,1,1,1)
+		state_machine.transition_to("Idle")
+	
+	if owner.detected_player == true and owner.distance_to_player >= 200:
+		hitbox.monitorable = false
+		animated_sprite.modulate = Color(1,1,1,1)
+		state_machine.transition_to("Run")
+		
+	if not owner.on_floor():
+		hitbox.monitorable = false
+		animated_sprite.modulate = Color(1,1,1,1)
+		state_machine.transition_to("Fall")
+		return 
+		
+	if owner.detected_player == false:
+		hitbox.monitorable = false
+		animated_sprite.modulate = Color(1,1,1,1)
+		state_machine.transition_to("Idle")
+	
---
+++ Enemies/Boss3/BOSS3.gd
@@ -0,0 +1,100 @@
+extends KinematicBody2D
+
+
+var speed = 150
+#var jump_impulse = 700 
+#var maximum_jumps := 2 
+var gravity = 2500
+var acceleration = 60
+var friction = 20 
+var air_friction = 10
+
+var knockback_x = 40
+var knockback_y = 2
+var threat_position = 0
+
+var health = 4
+
+var dead:bool = health <= 0
+
+#var slide_speed = 500
+#var dodge_speed = 400
+
+#var MAX_SPEED = 2000
+
+#const CHAIN_PULL = 50
+#var chain_velocity:= Vector2.ZERO
+
+var velocity := Vector2.ZERO
+#var jumps_made = 0
+
+var player_behind = false
+var detected_player = false
+var player_position = Vector2.ZERO
+var distance_to_player = 9999
+
+var taking_damage = false
+
+
+onready var ground_ray = get_node("ground_check_ray")
+onready var ground_ray2 = get_node("ground_check_ray2")
+onready var ground_ray3 = get_node("ground_check_ray3")
+
+var starting_directions = [-1,1]
+
+func _ready():
+	var starting_direction = starting_directions[randi() % starting_directions.size()]
+	if starting_direction < 0:
+		$AnimatedSprite.flip_h = true
+		$Vision.rotation_degrees = -50
+		$behindRay.rotation_degrees = -90
+		$EnemyHitbox.rotation_degrees = 90
+	
+	elif starting_direction > 0: 
+		$AnimatedSprite.flip_h = false
+		$Vision.rotation_degrees = 150
+		$behindRay.rotation_degrees = 90
+		$EnemyHitbox.rotation_degrees = 0
+		
+
+func set_direction() -> float:
+	
+	var direction = velocity.x
+	
+	if direction < 0:
+		$AnimatedSprite.flip_h = true
+		$Vision.rotation_degrees = -50
+		$behindRay.rotation_degrees = -90
+		$EnemyHitbox.rotation_degrees = 90
+		
+	if direction > 0: 
+		$AnimatedSprite.flip_h = false
+		$Vision.rotation_degrees = 150
+		$behindRay.rotation_degrees = 90
+		$EnemyHitbox.rotation_degrees = 0
+		
+	return direction 
+
+
+# HIT AND HURT BOXES, KNOCKBACK, BEHIND RAYCAST FUNCTION
+func on_floor() -> bool:
+	if (ground_ray.is_colliding() or 
+		ground_ray2.is_colliding() or
+		ground_ray3.is_colliding()):
+		return true
+	else:
+		return false
+
+func _on_EnemyHurtbox_area_entered(hitbox):
+	if hitbox.name == "PlayerHitbox" or hitbox.name == "PlayerHitbox2":
+		if hitbox.name == "PlayerHitbox2":
+			self.knockback_x *= 10
+		self.health -= hitbox.damage
+		self.taking_damage = true
+		self.dead = health <= 0
+		print(hitbox.get_parent().name + " dealing " + str(hitbox.damage) + " to " + name,"health: ",health,"dead: ", dead)
+		
+		self.threat_position = hitbox.global_position.x
+		#print(self.position.x,"  enemy: ", hitbox.global_position.x)
+
+#there is bug in death
