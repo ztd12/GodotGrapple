ae160a5a4eedb2ee56140bfd7e7ff35f26164027
varunika320 <66332471+varunika320@users.noreply.github.com>
2022-11-30 13:02:36 -0600

    hearts glitch fixed

--- SpaceLevel/SpaceLevel.gd
+++ SpaceLevel/SpaceLevel.gd
@@ -70,9 +70,13 @@ func _on_PauseButton_pressed():
 	_pause.popup()
 
 func load_hearts():
+	if Global.lives <= 0:
+		Global.lives = 0
+		$hearts.hide()
 	$hearts.rect_size.x = Global.lives * 600
-	$unfilledhearts.rect_size.x = (Global.max_lives - Global.lives) * 305
-	$unfilledhearts.rect_position.x = $hearts.rect_position.x + $hearts.rect_size.x * $hearts.rect_scale.x
+	#$hearts.rect_size.x = Global.lives * 600
+	#$unfilledhearts.rect_size.x = (Global.max_lives - Global.lives) * 305
+	#$unfilledhearts.rect_position.x = $hearts.rect_position.x + $hearts.rect_size.x * $hearts.rect_scale.x
 	
 
 
--- Tutorial/tutorial2b.gd
+++ Tutorial/tutorial2b.gd
@@ -8,6 +8,10 @@ func _ready():
 #func _on_Boss_tree_exited():
 
 func load_hearts():
-	$hearts.rect_size.x = Global.lives * 600
-	$unfilledhearts.rect_size.x = (Global.max_lives - Global.lives) * 305
-	$unfilledhearts.rect_position.x = $hearts.rect_position.x + $hearts.rect_size.x * $hearts.rect_scale.x
+	if Global.lives <= 0:
+		Global.lives = 0
+		$hearts.hide()
+	$hearts.rect_size.x = Global.lives * 600	
+	#$hearts.rect_size.x = Global.lives * 600
+	#$unfilledhearts.rect_size.x = (Global.max_lives - Global.lives) * 305
+	#$unfilledhearts.rect_position.x = $hearts.rect_position.x + $hearts.rect_size.x * $hearts.rect_scale.x
