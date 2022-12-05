f037a347c3e386d11baec549ba0b90dba6978cd6
olivia-Deegan <oliviadeegan@utexas.edu>
2022-12-05 09:32:54 -0600

    update tutorial

--- Tutorial/how_to_menu.gd
+++ Tutorial/how_to_menu.gd
@@ -5,3 +5,7 @@ extends Control
 func _on_homeButton_pressed():
 # warning-ignore:return_value_discarded
 	get_tree().change_scene("res://HUD/Main Menu.tscn")
+
+
+func _on_nextButton_pressed():
+	get_tree().change_scene("res://Tutorial/tutorial1.tscn")
--- Tutorial/nextButton1.gd
+++ Tutorial/nextButton1.gd
@@ -4,4 +4,4 @@ extends Button
 
 func _on_nextButton_pressed():
 # warning-ignore:return_value_discarded
-	get_tree().change_scene("res://Tutorial/tutorial1.tscn")
+	get_tree().change_scene("res://Tutorial/How_To_Menu_2.tscn")
